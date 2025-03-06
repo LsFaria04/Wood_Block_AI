bd6ef54cb913b31e704f01a76f73e984df4cdb09
Lucas Faria <lucasfaria@outlook.pt>
2025-03-06 19:38:09 +0000

    Added the greedy solution but it still needs testing

--- AppState.py
+++ AppState.py
@@ -1,5 +1,6 @@
 from GUI import GUI
 from GameState import GameState
+from AIPlayer import AIPlayer
 
 class AppState:
     #state 1 -> main menu
@@ -9,13 +10,15 @@ class AppState:
     def __init__(self):
         self.state = 2
         self.gui = GUI(1270, 720, "Wood Block")
-        self.game_state = GameState(50) # deve ser alterado no menu dependendo da setting
+        self.game_state = GameState(50,[]) # deve ser alterado no menu dependendo da setting
+        self.player = AIPlayer(5) #Use the greedy for testing
 
         
     
     def step(self):
         #Prepara o proximo passo no frame
         #precisa ser atualizado
+        self.player.play(self.game_state)
         self.game_state.drawBoard(self.gui)
         self.gui.refreshScreen()
         event = self.gui.getEvent()
--- GameState.py
+++ GameState.py
@@ -2,13 +2,15 @@ from collections import deque
 import numpy as np
 from PieceFactory import PieceFactory
 from Piece import Piece
+
 class GameState:
-    def __init__(self, boardSize) :
-        self.board = np.fromfunction(lambda i, j: (i % 2), (boardSize, boardSize), dtype=int)
+    def __init__(self, board_size, move_history) :
+        self.board = np.zeros((board_size, board_size))
         self.Q = deque() #where the pieces will be stored by order
         self.L = [] # where the pieces that the player can choose are stored
         self.piece = None  # Initialize self.piece
         self.piece_factory = PieceFactory()
+        self.move_history = move_history
         self.generatePieces()
         if self.L:
             self.piece = self.L[0]  # Initialize self.piece with the first piece in the list
@@ -31,7 +33,121 @@ class GameState:
                         if (x == px and y == py):
                             gui.drawPiece(self.piece, x * board_size, y * board_size, board_size)
 
+    def children(self):
+        '''
+        Returns all the possible moves with the pieces for selection
+        '''
+        lin_size = len(self.board)
+        col_size = len(self.board[0])
+
+        children = []
+
+        for piece_idx in range(len(self.L)):
+            for y in range(lin_size):
+                for x in range(col_size):
+                    if self.board[y][x] != 0:
+                        continue
+                    if self.is_move_possible(piece_idx, (x,y)):
+                        child = GameState(lin_size, self.move_history)
+                        child.board = self.board
+                        child.move(piece_idx, (x,y))
+                        print(child.board)
+                        children.append(child)
+
+        return children
+    
+    def is_move_possible(self, piece_idx, cords):
+        '''Checks if a move is possible for a given piece and board coordinates'''
+        lin_size = len(self.board)
+        col_size = len(self.board[0])
+
+        if piece_idx > 2 or piece_idx < 0:
+            # invalid index for the list of selection pieces
+            return False
         
+        piece = self.L[piece_idx]
+
+        x,y = cords
+
+        if y + piece.ylen >= lin_size :
+            return False
+        if x + piece.xlen >= col_size:
+            return False
+
+        for y_offset in range(piece.ylen):
+            for x_offset in range(piece.xlen):
+                if self.board[y + y_offset][x + x_offset] == 1 and piece.matrix[y_offset][x_offset] == 1:
+                    return False
+
+        return True
+
+    def move(self, piece_idx, cords):
+        '''Executes a move updating the game board with the given piece in the given coordinates. Assumes that the move is possible !!!!'''
+        x,y = cords
+        piece = self.L[piece_idx]
+
+        for y_offset in range(piece.ylen):
+            for x_offset in range(piece.xlen):
+                if piece.matrix[y_offset][x_offset] == 1:
+                    self.board[y + y_offset][x + x_offset] = 1
+        
+        self.move_history.append((piece, cords))
+
+        #inserts a new piece into the list of pieces that the player can play or removes it from the list if there aren't any more pieces
+        if len(self.Q) > 0:
+            self.L[piece_idx] = self.Q.popleft()
+        else:
+            self.L.pop(piece_idx)
+        
+        #checks and remove the lines and columns that are full after the move
+        full_lin, full_col = self.full_lines_columns()
+        self.remove_full_lines_columns(full_lin, full_col)
+    
+    def full_lines_columns(self):
+        full_lines = []
+        full_columns = []
+        line_numb = len(self.board)
+        col_numb = len(self.board[0])
+        lines = np.zeros(line_numb)
+        columns = np.zeros(col_numb)
+
+        #find how many cells are occupied in each line and column
+        for lin in range(line_numb):
+            for col in range(col_numb):
+                if self.board[lin][col] == 1:
+                    lines[lin] += 1
+                    columns[col] += 1
+        
+        #check what are the line that are full
+        for lin in range(line_numb):
+            if lines[lin] == col_numb:
+                full_lines.append(lin)
+        for col in range(col_numb):
+            if columns[col] == line_numb:
+                full_columns.append(col)
+
+        return (full_lines,full_columns)
+    
+    def remove_full_lines_columns(self, full_lines, full_columns):
+        lin_size = len(self.board)
+        col_size = len(self.board[0])
+
+        #remove the lines
+        for col in range(col_size):
+            for lin in full_lines:
+                self.board[lin][col] = 0
+        
+        #remove the columns
+        for lin in range(lin_size):
+            for col in full_columns:
+                self.board[lin][col] = 0
+    
+    
+    def game_over(self):
+        # no pieces to play 
+        return len(self.L) == 0 and len(self.Q) == 0
+
+
 
 
     
